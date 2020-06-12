eb53a559cb2c39d82ea65a05c0eda2fe7d2b9b11
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2020-06-12 09:50:50 +0800

    Fresh Start

---
+++ CodeForces/887a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, a, n) for (int i = a; i < n; ++i)
+#define dbg(x) cout << (#x) << " is " << x << "\n"
+using namespace std;
+
+string s;
+int cnt = 0;
+
+int main()
+{
+  cin >> s;
+  rep(i, 0, s.size()) {
+    if (s[i] == '1') {
+      rep(j, i+1, s.size())
+        if (s[j] == '0')
+          ++cnt;
+      break;
+    }
+  }
+  printf(cnt >= 6 ? "yes\n" : "no\n");
+
+  return 0;
+}
