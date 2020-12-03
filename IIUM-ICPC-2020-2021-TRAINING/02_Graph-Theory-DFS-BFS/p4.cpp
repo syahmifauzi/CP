96b161e7f1ef253bf8de00f528210e7010c0f1a4
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2020-12-04 01:16:07 +0800

    p4 solved using dfs

--- IIUM-ICPC-2020-2021-TRAINING/02_Graph-Theory-DFS-BFS/p4.cpp
+++ IIUM-ICPC-2020-2021-TRAINING/02_Graph-Theory-DFS-BFS/p4.cpp
@@ -27,9 +27,34 @@ typedef vector<ll> vl;
 typedef vector<pll> vll;
 ld EPS = 1e-12;
 
+int n, m, cats[100010], res;
+bool vis[100010];
+vector<int> adjList[100010];
+
+void dfs(int u, int totCats) {
+  // cout << "Currently at node " << u << " with totCats = " << totCats << endl;
+  vis[u] = 1;
+  if (totCats > m) return;
+  if (adjList[u].size() == 1 && u != 1) ++res;
+  for (auto v: adjList[u]) {
+    if (vis[v] == 0)
+      dfs(v, (totCats + cats[v]) * cats[v]);
+  }
+}
+
 int main()
 {
-  printf("Start!\n");
+  memset(vis, 0, sizeof vis);
+  s(n); s(m);
+  rep(i, 1, n+1) s(cats[i]);
+  rep(i, 0, n-1) {
+    int u, v;
+    s(u), s(v);
+    adjList[u].pb(v);
+    adjList[v].pb(u);
+  }
+  dfs(1, cats[1]);
+  p(res); pnl();
 
   return 0;
 }
